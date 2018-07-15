0762242709e58aa5941bc803a9c5cd9db74b2d15
entrox <entrox@stoleyour.com>
2018-07-15 03:34:19 -0400

    small role check to avoid it being spammed by random people

--- ouo.py
+++ ouo.py
@@ -9,14 +9,15 @@ class Shorten:
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def ouo(self, url):
-
-        url2 = ("http://ouo.io/api/B3Nm9g8L?s=" + url)
-        handle = urllib.request.Request(url2, headers={'User-Agent': 'Mozilla/5.0'})
-        html = urllib.request.urlopen(handle).read().decode('utf-8')
-        await self.bot.say("Shortened: " + html)
-        await asyncio.sleep(1)
+    @commands.command(pass_context=True)
+    async def ouo(self, ctx):
+        if "465221738976772108" in [role.id for role in ctx.message.author.roles]:
+            url = ("http://ouo.io/api/B3Nm9g8L?s=" + ctx.message.content)
+            handle = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+            html = urllib.request.urlopen(handle).read().decode('utf-8')
+            await self.bot.say("Here you go :link: " + html)
+        else:
+            await self.bot.say("I do not have a API key for your user ID.")
 
 def setup(bot):
     bot.add_cog(Shorten(bot))
