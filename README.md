b3d0bad59f3e813daf44af0873259bbee937e2be
entrox <entrox@stoleyour.com>
2018-07-16 17:01:32 -0400

    fixed & and # in URL's + urle support

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # discordbot-cogs
+#
+#Database format is {"USERID":"APIKEY","117450987848990720":"B3Nm9g8L?s="}
+#
--- urle.py
+++ urle.py
@@ -6,26 +6,26 @@ import json
 import os.path
 from discord.ext import commands
 
-class ShortenUrlouo:
+class ShortenUrlurle:
 
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command(pass_context=True)
-    async def ouo(self, ctx):
+    async def urle(self, ctx):
         if "465221738976772108" in [role.id for role in ctx.message.author.roles]:
             if os.path.isfile('data/ouo/users.json'):
-                with open('data/ouo/users.json') as user_file:
+                with open('data/urle/users.json') as user_file:
                     users_loaded = json.load(user_file)
 
                     if users_loaded.get(ctx.message.author.id):
-                        storedURL = ctx.message.content[5:]
+                        storedURL = ctx.message.content[6:]
                         storedURL = storedURL.replace("&", "%26")
                         storedURL = storedURL.replace("#", "%23") 
-                        url = ("http://ouo.io/api/" + users_loaded.get(ctx.message.author.id) + storedURL)
+                        url = ("https://urle.co/api/?api=" + users_loaded.get(ctx.message.author.id) + "&format=text&url=" + storedURL)
                         urlRequest = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})          
                         shortenedUrl = urllib.request.urlopen(urlRequest).read().decode('utf-8')
-                        await self.bot.say("Here you go :link: " + shortenedUrl)
+                        await self.bot.say("Here you go :link: <" + shortenedUrl">")
                     else:
                         await self.bot.say("API key missing.")
             else:
@@ -34,4 +34,4 @@ class ShortenUrlouo:
             await self.bot.say("You are not allowed to use this command.")
 
 def setup(bot):
-    bot.add_cog(ShortenUrlouo(bot))
+    bot.add_cog(ShortenUrlurle(bot))
