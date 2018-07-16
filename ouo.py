edcf73b4362c5c14f45ffbc0bb739d3c0d2f3664
entrox <entrox@stoleyour.com>
2018-07-15 23:16:43 -0400

    read userid/api from local file

--- ouo.py
+++ ouo.py
@@ -2,9 +2,11 @@ import discord
 import asyncio
 import urllib.request
 import requests
+import json
+import os.path
 from discord.ext import commands
 
-class Shorten:
+class ShortenUrl:
 
     def __init__(self, bot):
         self.bot = bot
@@ -12,25 +14,18 @@ class Shorten:
     @commands.command(pass_context=True)
     async def ouo(self, ctx):
         if "465221738976772108" in [role.id for role in ctx.message.author.roles]:
-            if "71597167550144512" == ctx.message.author.id:
-                url = ("http://ouo.io/api/B3Nm9g8L?s=" + ctx.message.content)
-                handle = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-                html = urllib.request.urlopen(handle).read().decode('utf-8')
-                await self.bot.say("Here you go :link: " + html)
-            elif "465187211118116875" == ctx.message.author.id:
-                url = ("http://ouo.io/api/rNKDAnkJ?s=" + ctx.message.content)
-                handle = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-                html = urllib.request.urlopen(handle).read().decode('utf-8')
-                await self.bot.say("Here you go :link: " + html)
-            elif "403892685473185792" == ctx.message.author.id:
-                url = ("http://ouo.io/api/oS9w9Nae?s=" + ctx.message.content)
-                handle = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-                html = urllib.request.urlopen(handle).read().decode('utf-8')
-                await self.bot.say("Here you go :link: " + html)
-            else:
-                await self.bot.say("API key missing.")   
+            with open('data/ouo/users.json') as user_file:
+                users_loaded = json.load(user_file)
+
+                if users_loaded.get(ctx.message.author.id):
+                    url = ("http://ouo.io/api/" + users_loaded.get(ctx.message.author.id) + ctx.message.content[5:])
+                    urlRequest = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})          
+                    shortenedUrl = urllib.request.urlopen(urlRequest).read().decode('utf-8')
+                    await self.bot.say("Here you go :link: " + shortenedUrl)
+                else:
+                    await self.bot.say("API key missing.")
         else:
             await self.bot.say("You are not allowed to use this command.")
 
 def setup(bot):
-    bot.add_cog(Shorten(bot))
+    bot.add_cog(ShortenUrl(bot))
